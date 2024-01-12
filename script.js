c8ab4e69bbe878e3a414dedd3b06427f3e326c42
ZaidMadanat <zaidsmadanat@gmail.com>
2024-01-12 14:17:40 -0800

    finished js, tbd statistics, style

--- script.js
+++ script.js
@@ -1,24 +1,48 @@
 const myLibrary = [];
 
-function Book(title, author, pages, isRead) { 
+const book_name = document.querySelector('#book_name');
+const book_author = document.querySelector('#author');
+const book_pages = document.querySelector('#pages');
+const book_rating = document.querySelector('#rating');
+const book_genre = document.querySelector('#genre');
+const book_status = document.querySelector('#status');
+
+const bookForm = document.getElementById('bookForm')
+bookForm.addEventListener('submit', function(e) { 
+    e.preventDefault();
+    addBookToLibrary();
+    bookForm.reset();
+    displayBooks();
+});
+
+
+
+function Book(title, author, pages, rating, genre, isRead) { 
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.rating = rating;
+    this.genre = genre;
     this.isRead = isRead;
-    this.read = function() { 
-        if (this.isRead == false) { 
-            return "not read";
-        }
-        else { 
-            return "read";
-        }
-    }
-
-    this.info = function() { 
-        return this.title + " by " +  this.author + ", " + this.pages + " pages, " + this.read();
-    }
 }
 
 function addBookToLibrary() { 
-    
+    const newBook = new Book(book_name.value, book_author.value, book_pages.value ,book_rating.value ,book_genre.value ,book_status.value);
+    myLibrary.push(newBook);
+}
+function displayBooks() { 
+    const books = document.querySelector('.books');
+    myLibrary.forEach(book => { 
+        const bookCard = document.createElement('div');
+        bookCard.classList.add('card');
+        bookCard.innerHTML = `
+        <h3>${book.title}</h3>
+        <p>Author: ${book.author}</p>
+        <p>Pages: ${book.pages}</p>
+        <p>Rating: ${book.rating}</p>
+        <p>Genre: ${book.genre}</p>
+        <p>Status: ${book.isRead}</p> 
+        ` 
+        books.appendChild(bookCard);
+    })
 }
